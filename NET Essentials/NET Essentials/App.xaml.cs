366d0147c593c0e4d5d318c623459ca3511d22ff
Andrii Poznanskyi <andrii.poznanskyi@lynceetec.com>
2024-06-18 22:39:39 +0300

    First commit, create project

---
+++ NET Essentials/NET Essentials/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NET_Essentials
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ NET Essentials/NET Essentials/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NET_Essentials
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
